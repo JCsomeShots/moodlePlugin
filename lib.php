8007c5e71a94ba847041d4b0bc610874ae3223d0
JuanCarlo Castillo <juancarlo.castillo20@gmail.com>
2022-08-29 11:16:03 +0200

    Add settings page

--- lib.php
+++ lib.php
@@ -25,6 +25,56 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+
+/**
+ * Insert a link to index.php on the site front page navigation menu.
+ *
+ * @param navigation_node $frontpage Node representing the front page in the navigation tree.
+ */
+function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
+    if (get_config('local_greetings', 'showinnavigation')) {
+        if (isloggedin() && !isguestuser()) {
+            $frontpage->add(
+                get_string('pluginname', 'local_greetings'),
+                new moodle_url('/local/greetings/index.php'),
+                navigation_node::TYPE_CUSTOM,
+                null,
+                null,
+                new pix_icon('t/message', '')
+            );
+        }
+    }
+}
+
+/**
+ * Add link to index.php into navigation drawer.
+ *
+ * @param global_navigation $root Node representing the global navigation tree.
+ */
+function local_greetings_extend_navigation(global_navigation $root) {
+    if (isloggedin() && !isguestuser()) {
+        $node = navigation_node::create(
+            get_string('pluginname', 'local_greetings'),
+            new moodle_url('/local/greetings/index.php'),
+            navigation_node::TYPE_CUSTOM,
+            null,
+            null,
+            new pix_icon('t/message', '')
+        );
+
+        $node->showinflatnavigation = get_config('local_greetings', 'showinnavigation');
+
+        $root->add_node($node);
+    }
+}
+
+
+/**
+ * Get a localised greeting message for a user
+ *
+ * @param \stdClass $user
+ * @return string
+ */
 /** Create a personal greetings and check the country. */
 function local_greetings_get_greeting($user) {
     if ($user == null) {
@@ -52,38 +102,4 @@ function local_greetings_get_greeting($user) {
     }
 
     return get_string($langstr, 'local_greetings', fullname($user));
-}
-
-
-/**
- * Insert a link to index.php on the site front page navigation menu.
- *
- * @param navigation_node $frontpage Node representing the front page in the navigation tree.
- */
-function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
-
-    $frontpage->add(
-        get_string('pluginname', 'local_greetings'),
-        new moodle_url('/local/greetings/index.php'),
-        navigation_node::TYPE_CUSTOM,
-        null,
-        null,
-        new pix_icon('t/message', '')
-    );
-}
-
-/** add a link to navigation drawer */
-function local_greetings_extend_navigation(global_navigation $root) {
-
-    $node = navigation_node::create(
-        get_string('pluginname', 'local_greetings'),
-        new moodle_url('/local/greetings/index.php'),
-        navigation_node::TYPE_CUSTOM,
-        null,
-        null,
-        new pix_icon('t/message', '')
-    );
-
-    $node->showinflatnavigation = true;
-    $root->add_node($node);
 }
